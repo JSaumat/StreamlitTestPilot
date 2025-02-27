6adfd8dc0dea488e8badfb290e8beb4a2cde6a90
JSaumat <j_saumat@mail.fhsu.edu>
2025-02-26 20:06:46 -0700

    Playing with streamlit while following tutorial

---
+++ dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("This is the title")
+
+st.header("This is the header")
+
+st.subheader("This is the subheader")
+st.write("This is regular text")
+
+"""
+# header
+## subheader
+"""
+
+{
+    "key": "value",
+    "key2": "value2"
+}
