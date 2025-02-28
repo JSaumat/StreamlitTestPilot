d0b81c8d5ae25211ae7041baaf20afb53b0f1531
JSaumat <j_saumat@mail.fhsu.edu>
2025-02-27 19:57:29 -0700

    Finished following the tutorial.

--- dashboard.py
+++ dashboard.py
@@ -5,15 +5,14 @@ import numpy as np
 import tweepy
 import config
 import psycopg2, psycopg2.extras
+import plotly.graph_objects as go
 
 auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
 auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-option = st.sidebar.selectbox(
-    "Which dashboard?", ("twitter", "wallstreetbets", "stocktwits", "chart", "pattern"),
-)
+option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'), 3)
 
 st.header(option)
 
@@ -38,7 +37,29 @@ if option == "twitter":
                     st.image(f"https://finviz.com/chart.ashx?t={symbol}")
 
 if option == "chart":
-    st.subheader("this is the chart dashboard")
+    symbol = st.sidebar.text_input("Symbol", value='MSFT', max_chars=None, key=None, type='default')
+
+    data = pd.read_sql("""
+        select date(day) as day, open, high, low, close
+        from daily_bars
+        where stock_id = (select id from stock where UPPER(symbol) = %s) 
+        order by day asc""", connection, params=(symbol.upper(),))
+
+    st.subheader(symbol.upper())
+
+    fig = go.Figure(data=[go.Candlestick(x=data['day'],
+                                         open=data['open'],
+                                         high=data['high'],
+                                         low=data['low'],
+                                         close=data['close'],
+                                         name=symbol)])
+
+    fig.update_xaxes(type='category')
+    fig.update_layout(height=700)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.write(data)
 
 if option == "wallstreetbets":
 
@@ -124,7 +145,51 @@ if option == 'pattern':
         st.image(f"https://finviz.com/chart.ashx?t={row['symbol']}")
 
 if option == "pattern":
-    st.subheader("pattern")
+    pattern = st.sidebar.selectbox(
+        "Which Pattern?",
+        ("engulfing", "threebar")
+    )
+
+    if pattern == 'engulfing':
+        cursor.execute("""
+            SELECT * 
+            FROM ( 
+                SELECT day, open, close, stock_id, symbol, 
+                LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
+                LAG(open, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_open 
+                FROM daily_bars
+                JOIN stock ON stock.id = daily_bars.stock_id
+            ) a 
+            WHERE previous_close < previous_open AND close > previous_open AND open < previous_close
+            AND day = '2021-02-18'
+        """)
+
+    if pattern == 'threebar':
+        cursor.execute("""
+            SELECT * 
+            FROM ( 
+                SELECT day, close, volume, stock_id, symbol, 
+                LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
+                LAG(volume, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_volume, 
+                LAG(close, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_close, 
+                LAG(volume, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_volume, 
+                LAG(close, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_close, 
+                LAG(volume, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_volume 
+            FROM daily_bars 
+            JOIN stock ON stock.id = daily_bars.stock_id) a 
+            WHERE close > previous_previous_previous_close 
+                AND previous_close < previous_previous_close 
+                AND previous_close < previous_previous_previous_close 
+                AND volume > previous_volume 
+                AND previous_volume < previous_previous_volume 
+                AND previous_previous_volume < previous_previous_previous_volume 
+                AND day = '2021-02-19'
+        """)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        st.image(f"https://finviz.com/chart.ashx?t={row['symbol']}")
 
 if option == "stocktwits":
 
